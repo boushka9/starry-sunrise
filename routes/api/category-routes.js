6705104f6ce2793b4b1b18ee789afec8804db0e0
Danny Boushka <boushka9@gmail.com>
2023-04-09 14:46:36 -0600

    Added PUT route for Categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -80,6 +80,16 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((categoryData) => res.json(categoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
